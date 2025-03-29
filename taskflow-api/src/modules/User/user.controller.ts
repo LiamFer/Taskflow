eab71d7b4ce9957f82cfdceaabe7b679a9a85767
William Fernandes <wiilfern1910@outlook.com>
2025-03-29 14:08:41 -0300

    Gerando o JWT no Login

--- taskflow-api/src/modules/User/user.controller.ts
+++ taskflow-api/src/modules/User/user.controller.ts
@@ -10,27 +10,67 @@ import {
 import { UserService } from './user.service';
 import { Response } from 'express';
 import { ResponseUtil } from 'src/utils/response';
+import { AuthService } from '../Auth/auth.service';
 
 // USER ROUTE
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private authService: AuthService,
+  ) {}
 
   @Post('register')
   Register(@Body() body: any, @Res() res: Response): object {
     try {
       console.log(body);
       this.userService.createUser(body.name, body.email, body.password);
-      return ResponseUtil.sendResponse(res,HttpStatus.ALREADY_REPORTED,'User Created Successfully!')
+      return ResponseUtil.sendResponse(
+        res,
+        HttpStatus.CREATED,
+        'User Created Successfully!',
+      );
     } catch (error) {
       return { statusCode: HttpStatus.BAD_REQUEST, message: error.message };
     }
   }
 
   @Post('login')
-  Login(@Body() body: any): object {
-    console.log(body);
-    this.userService.createUser(body.name, body.email, body.password);
-    return { message: 'User Created!' };
+  async Login(@Body() body: any, @Res() res: Response): Promise<object> {
+    const authUser = await this.userService.authenticateUser(
+      body.email,
+      body.password,
+    );
+
+    if (authUser) {
+      // Gerando o JWT
+      const jwtToken = this.authService.generateToken(body);
+
+      // Definindo as opções do cookie
+      const cookieOptions = {
+        name: 'jwt', 
+        value: jwtToken,
+        options: {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production', 
+          maxAge: 3600000, // 1 hora de expiração
+        },
+      };
+
+      return ResponseUtil.sendResponse(
+        res,
+        HttpStatus.ACCEPTED,
+        'User Logged!',
+        null, 
+        null, 
+        cookieOptions,
+      );
+    } else {
+      return ResponseUtil.sendResponse(
+        res,
+        HttpStatus.NOT_FOUND,
+        'User not Found!',
+      );
+    }
   }
 }
--- taskflow-api/src/modules/User/user.service.ts
+++ taskflow-api/src/modules/User/user.service.ts
@@ -4,6 +4,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/database/entities/user.entity';
 import { Repository } from 'typeorm';
 import { AuthService } from '../Auth/auth.service';
+import { user } from 'src/interfaces/userInterface';
 
 @Injectable()
 export class UserService {
@@ -17,12 +18,30 @@ export class UserService {
     email: string,
     password: string,
   ): Promise<User> {
-    const hashedPassword = await this.authService.hashPassword(password)
-    const user = this.userRepository.create({ name, email, password:hashedPassword });
+    const hashedPassword = await this.authService.hashPassword(password);
+    const user = this.userRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
     return await this.userRepository.save(user);
   }
 
   async getUsers(): Promise<User[]> {
     return this.userRepository.find();
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.userRepository.findOneBy({ email });
+  }
+
+  async authenticateUser(email: string, password: string): Promise<boolean> {
+    const user: user | null = await this.findByEmail(email);
+
+    if (!user) {
+      return false;
+    } else {
+      return await this.authService.comparePassword(password, user.password);
+    }
   }
 }
