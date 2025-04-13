7c6bffb27df92b2162dc0d312c1e4896a9c090d3
William Fernandes <wiilfern1910@outlook.com>
2025-04-13 09:08:59 -0300

    App Theme no Local Storage

--- Client/src/Context/themeContext.jsx
+++ Client/src/Context/themeContext.jsx
@@ -1,9 +1,22 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const themeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
   const [darkMode, setDarkMode] = useState(true);
+
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    if (theme) {
+      setDarkMode(theme == "dark" ? true : false);
+    } else {
+      localStorage.setItem("theme", darkMode ? "dark" : "light");
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("theme", darkMode ? "dark" : "light");
+  }, [darkMode]);
 
   return (
     <themeContext.Provider value={{ darkMode, setDarkMode }}>
